334f1e9a562d5e2d3f3ef439a7e915aaecec4d53
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2022-01-30 13:31:37 -0500

    add hbs support

--- src/js/post/index.js
+++ src/js/post/index.js
@@ -4,20 +4,15 @@ import javascript from 'highlight.js/lib/languages/javascript';
 import css from 'highlight.js/lib/languages/css';
 import html from 'highlight.js/lib/languages/xml';
 import bash from 'highlight.js/lib/languages/bash';
+import handlebars from 'highlight.js/lib/languages/handlebars';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('css', css);
 hljs.registerLanguage('html', html);
 hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('handlebars', handlebars);
 
 hljs.highlightAll();
-// const hljs = require('highlight.js/lib/common');
-
-// import hljs from 'highlight.js/lib/common';
-// import javascript from 'highlight.js/lib/languages/javascript';
-// import handlebars from 'highlight.js/lib/languages/handlebars';
-// import css from 'highlight.js/lib/languages/css';
-// console.log(hljs);
 
 import { toc } from './toc';
 import { copyHandler } from './copy';
