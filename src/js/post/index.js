ae814c0c1839b6cb60267b32a2cf4cb8db3bdb05
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2022-02-25 09:09:27 -0500

    add support for json

--- src/js/post/index.js
+++ src/js/post/index.js
@@ -5,12 +5,14 @@ import css from 'highlight.js/lib/languages/css';
 import html from 'highlight.js/lib/languages/xml';
 import bash from 'highlight.js/lib/languages/bash';
 import handlebars from 'highlight.js/lib/languages/handlebars';
+import json from 'highlight.js/lib/languages/json';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('css', css);
 hljs.registerLanguage('html', html);
 hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('handlebars', handlebars);
+hljs.registerLanguage('json', json);
 
 hljs.highlightAll();
 
